3578fb5237f2a6c22874b875b06a64cdf51f32e4
Wassim2026 <wassimabbasbalouch@gmail.com>
2026-09-18 20:17:00 +0400

    Add Meta landing-page prize wheel and giveaway checkout
    * Add Meta prize wheel giveaway flow
    
    * Add Meta prize wheel giveaway flow
    
    * Add Meta prize wheel giveaway flow
    
    * Add Meta prize wheel giveaway flow
    
    * Add Meta prize wheel giveaway flow
    
    * Add Meta prize wheel giveaway assets
    
    * Add Meta prize wheel giveaway assets
    
    * Add Meta prize wheel giveaway assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install',function(){self.skipWaiting();});
+self.addEventListener('activate',function(event){event.waitUntil(self.clients.claim());});
+self.addEventListener('notificationclick',function(event){
+  event.notification.close();
+  event.waitUntil(self.clients.matchAll({type:'window',includeUncontrolled:true}).then(function(clients){
+    for(var i=0;i<clients.length;i++){if('focus' in clients[i])return clients[i].focus();}
+    if(self.clients.openWindow)return self.clients.openWindow('/shop.html');
+  }));
+});
+
